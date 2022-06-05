7cb3313a2bfb64a4425b5429bc5ac39ed234d1a0
Mohammad Hasan <hm91hassan@gmail.com>
2022-06-05 19:02:59 +0600

    update readme

--- config/safebrowsing.php
+++ config/safebrowsing.php
@@ -4,8 +4,8 @@ return [
 
 
 
-    'api_key' => env('GOOGLE_API_KEY','AIzaSyCjyQws6nKzCEN9oyA7OCoVOxorDqupEcQ'),
-    'client_id' => env('GOOGLE_CLIENT_ID','save-browsing-352304')
+    'api_key' => env('GOOGLE_API_KEY',''),
+    'client_id' => env('GOOGLE_CLIENT_ID','')
 
 
 
